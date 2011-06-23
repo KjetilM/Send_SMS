6cd5bee7dc9c7de2e545a937bbb565d0627e9067
Kjetil Maeland <kjetil@pswin.com>
2011-06-23 15:46:01 +0200

    added notes in README and first .ps1 file

---
+++ SMS.ps1
@@ -0,0 +1,23 @@
+﻿Function SendSMS
+{
+	Param
+	(
+		[string]$UserName = $(Throw 'No username provided'),
+		[string]$Password = $(Throw 'No Password provided'),
+		[string]$Receiver = $(Throw 'No Receivernumber provided'),
+		[string]$Url = 'http://gw2-fro.pswin.com:81/',
+		[string]$Message,
+		[boolean]$Debug
+		
+	)
+	$XML = "<SESSION><CLIENT>$UserName</CLIENT><PW>$Password</PW><MSGLST><MSG><ID>1</ID><TEXT>$Message</TEXT><RCV>$Receiver</RCV></MSG></MSGLST></SESSION>"
+	If($Debug -eq 'TRUE')
+	{
+		write-host $XML
+	}
+	$Http = New-Object System.Net.WebClient
+	$HttpResponse = $Http.UploadString($Url,'POST' ,$XML)
+	return $HttpResponse
+
+}
+
