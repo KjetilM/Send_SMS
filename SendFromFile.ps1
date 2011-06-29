9a722cc942c332ac11fb2f05e73d49f92fef5404
Kjetil Maeland <kjetil@pswin.com>
2011-06-29 11:12:12 +0200

    added files

---
+++ SendFromFile.ps1
@@ -0,0 +1,20 @@
+﻿#Example script that 'Dot Sources' the SMS_SendMulitiple.ps1 script file and uses functions from that script.
+#Supply your username password and path to file. ( SendFromfile.ps1 Username Password .\Messages.csv)
+#The file should be a CSV with Phonenumber,Message header then a list of Number,Message pairs.
+
+.{.\SMS_SendMulitiple.ps1} #You need to have the SMS_SendMulitiple.ps1 file in the same dir as this script. Or change the path here.
+
+$username = $args[0]
+$passord = $args[1]
+$CSVFile = $args[2]
+
+$Array_Messages = Import-Csv $CSVFile
+$MSG_List = @{}
+foreach($item in $Array_Messages)
+{
+	$MSG_OBJ = CreateMessageObjects -Receiver $item.Phonenumber.ToString() -Message_text $item.Message.ToString()
+	$MSG_List.add($MSG_List.Count + 1,$MSG_OBJ)
+}
+
+$Message_XML = BuildXML -UserName $username -Password $passord -HTBL_RCV_MSG $MSG_LIST
+sendSMS -XMLBLOCK $Message_XML[-1].outerxml.tostring()
---
+++ Sendsingle.ps1
@@ -0,0 +1,41 @@
+﻿#Example script that 'Dot Sources' the SMS_SendMulitiple.ps1 script file and uses functions from that script.
+#Usage of this script is simple ' SendSingle.ps1 [PhoneNumber] [Message]'
+
+
+
+
+.{.\SMS_SendMulitiple.ps1} #You need to have the SMS_SendMulitiple.ps1 file in the same dir as this script. Or change the path here.
+function SendSingle
+{
+	param
+	(
+		[string]$PhoneNumber,
+		[string]$Message,
+		[String]$UserName,
+		[String]$Password
+	)
+	
+	$HT = @{}
+	$MSG_OBJ = CreateMessageObjects -Receiver $PhoneNumber -Message_text $Message
+	$HT.add($HT.Count + 1,$MSG_OBJ)
+	
+	$Message_XML = BuildXML -UserName $UserName -Password $Password -HTBL_RCV_MSG $HT
+	
+	sendsms -XMLBLOCK $Message_XML[-1].outerxml.tostring()
+}
+
+
+if ($args.count -lt 4)
+{
+	Write-Host 'you need to supply four arguments ( phonenumber, message ,username and password)'
+	break
+}
+
+if ($args[0].tostring().Length -lt 8)
+{
+	Write-Host 'Phonenumber is not vallid, remeber to use countryprefix'
+	break
+}
+
+
+Sendsingle -PhoneNumber $args[0].tostring() -Message $args[1] -UserName $args[2] -Password $args[3]
